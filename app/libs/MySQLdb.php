9a57911bbd836054863e24d5495b48c6a4f4c6c0
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-30 22:00:47 -0500

    Creando una función query para poder realizar las consultas del sql apropiadamente

--- app/libs/MySQLdb.php
+++ app/libs/MySQLdb.php
@@ -14,9 +14,15 @@ class MySQLdb {
             $this->usuario,
             $this->clave
         );
-        echo "Conectado";
+        //echo "Conectado";
         } catch (Exception $e) {
             die("No se pudo conectar: " . $e->getMessage());
         }
+    }
+
+    public function query($sql=''){
+        if(empty($sql)) return false;
+        $stmt = $this->conn->query($sql);
+        return $stmt->fetch();
     }
 }
