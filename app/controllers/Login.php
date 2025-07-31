ab587648b2f21867454d718855ba8eeb74e4cf05
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-30 21:32:13 -0500

    Aplicando validaciones para el formulario de loginOlvidoVista

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -16,7 +16,28 @@ class Login extends Controlador{
        $this->vista("loginCaratulaVista", $datos);
     }
 
-    public function olvido() {
+    /* ?? -> validación de null */
+    public function olvidoVerificar() {
+        $errores = [];
+        if($_SERVER['REQUEST_METHOD'] == "POST"){
+            $usuario = $_POST['usuario']??""; 
+
+            // validación de que el correo no debe estar vacío
+            if(empty($usuario)){
+                array_push($errores, "El correo electrónico es requerido");
+            }
+
+            // validar el correo si esta bien descrito de acuerdo a sus estandares
+            if(filter_var($usuario, FILTER_VALIDATE_EMAIL) == false){
+                 array_push($errores, "El correo electrónico no está bien escrito");
+            }
+
+            if(empty($errores)){
+                Helper::mostrar($usuario);
+            }
+            Helper::mostrar($errores);
+
+        }
        $datos = [
         "titulo" => "Olvido de Contraseña",
         "subtitulo" => "¿Olvidaste tu contraseña?"
