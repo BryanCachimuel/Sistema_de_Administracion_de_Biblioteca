cfadd00f9d10d6a620a02f10151d2bd6bc87ad9a
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-30 19:51:03 -0500

    Instalando la librería bootstrap 5.3 y organizando las separaciones con la etiqueta div para crear un formulario

---
+++ app/views/loginCaratulaVista.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca | Entrada</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a href="#" class="navbar-brand">Biblioteca</a>
+    </nav>
+
+    <div class="container-fluid">
+        <div class="row content">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <table>
+                    <tr>
+                        <td>Usuario: </td>
+                        <td>
+                            <input type="text" name="usuario" id="usuario">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Clave: </td>
+                        <td>
+                            <input type="password" name="clave" id="clave">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="submit" value="Enviar">
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
+    </div>
+
+</body>
+</html>
