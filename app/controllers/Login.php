eaffba12d20a0b1c67e638b5f94c718b719e30e1
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-30 20:52:17 -0500

    Creando el método olvido para enviar hacia la vista datos y una ruta

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -8,8 +8,19 @@ class Login extends Controlador{
         $this->modelo = $this->modelo("LoginModelo");
     }
 
-    public function caratula($value='') {
-       $datos = [];
+    public function caratula() {
+       $datos = [
+        "titulo" => "Entrada a la Biblioteca",
+        "subtitulo" => "Sistema de Biblioteca"
+       ];
        $this->vista("loginCaratulaVista", $datos);
+    }
+
+    public function olvido() {
+       $datos = [
+        "titulo" => "Olvido de Contraseña",
+        "subtitulo" => "¿Olvidaste tu contraseña?"
+       ];
+       $this->vista("loginOlvidoVista", $datos);
     }
 }
