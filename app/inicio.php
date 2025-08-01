6aa0f51a8b6dbac547ad54499a81afc33e19413f
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-31 21:40:56 -0500

    En el archivo inicio se crean dos constantes para poder encriptar de dos formas y en el archivo Helper se crear dos funciones para encriptar y desencriptar los datos que se requiera

--- app/inicio.php
+++ app/inicio.php
@@ -2,6 +2,10 @@
 // control de las rutas mediante una constante
 define("RUTA", "/biblioteca/");
 
+// Llaves para encriptación de datos del usuario
+define("LLAVE1","Hombresneciosque");
+define("LLAVE2","acusaisalamujer");
+
 require_once("libs/MySQLdb.php");
 require_once("libs/Helper.php");
 require_once("libs/Controlador.php");
--- app/libs/Helper.php
+++ app/libs/Helper.php
@@ -10,4 +10,14 @@ class Helper {
         }
     }
 
+    public static function encriptar($data) {
+        return base64_encode(LLAVE1.$data.LLAVE2);
+    }
+
+    public static function desencriptar($data) {
+        $cadena = base64_decode($data);
+        $cadena = str_replace(LLAVE1,"",$cadena);
+        return str_replace(LLAVE2,"",$cadena);
+    }
+
 }
