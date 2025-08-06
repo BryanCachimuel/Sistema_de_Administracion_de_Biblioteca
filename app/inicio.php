a871891711cb3087deaa0c429df5a98b921d2366
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-05 22:28:36 -0500

    Adecuando ordenamente las constantes que se usarán en el sistema

--- app/inicio.php
+++ app/inicio.php
@@ -7,8 +7,29 @@ define("LLAVE1","Hombresneciosque");
 define("LLAVE2","acusaisalamujer");
 define("CLAVE","clavesecretamaxima");
 
+//
+define('ADMON',1);
+define('PROFESOR',2);
+define('ESTUDIANTE',3);
+define('EXTERNO',4);
+//
+//Estados usuario
+//
+define('USUARIO_ACTIVO',1);
+define('USUARIO_INACTIVO',2);
+define('USUARIO_SUSPENDIDO',3);
+//
+//Estado copias
+//
+define('DISPONIBLE',1);
+define('PRESTADO',2);
+define('REPARACION',3);
+define('PERDIDO',4);
+define('NO_DISPONIBLE',5);
+
 require_once("libs/MySQLdb.php");
 require_once("libs/Helper.php");
 require_once("libs/Sesion.php");
+require_once("libs/Llaves.php");
 require_once("libs/Controlador.php");
 require_once("libs/Control.php");
