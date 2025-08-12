625cb41d4a1ba7b2e6ea99e365fa9a587297f1bb
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-11 22:36:02 -0500

    Creando el método alta el cual cuenta con validaciones para poder crear un país y almacenarlo en la base de datos

--- app/controllers/Paises.php
+++ app/controllers/Paises.php
@@ -17,13 +17,40 @@ class Paises extends Controlador
 	{
 		$data = $this->modelo->getTabla();
 		$datos = [
-			"titulo" => "PaÍses",
-			"subtitulo" => "PaÍses",
+			"titulo" => "Paises",
+			"subtitulo" => "Paises",
 			"activo" => "paises",
 			"menu" => true,
 			"data" => $data
 		];
 		$this->vista("paisesCaratulaVista", $datos);
+	}
+
+	public function alta() {
+		// definir arreglos
+		$data = array();
+		$errores = array();
+
+		// recibir la información de la vista
+		if($_SERVER['REQUEST_METHOD']!="POST") {
+			$id = $_POST["id"]??"";
+			$pais = Helper::cadena($_POST['pais']??"");
+			Helper::mostrar($pais);
+		}
+
+		if(!empty($errores) || $_SERVER['REQUEST_METHOD']!="POST") {
+			//Vista de alta
+			$datos = [
+				"titulo" => "Alta de un País",
+				"subtitulo" => "Alta de un País",
+				"activo" => "paises",
+				"menu" => true,
+				"admon" => "admon",
+				"errores" => $errores,
+				"data" => []
+			];
+			$this->vista("paisesAltaVista", $datos);
+		}
 	}
 
 }
