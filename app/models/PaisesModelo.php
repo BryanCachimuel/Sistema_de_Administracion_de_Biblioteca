4d9f290759836160e37b9fc603a18adda0d3f69b
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-11 22:18:30 -0500

    Creando el método getTabla el cual cuenta con la consulta hacia la base de datos para obtener el listado completo de paises

--- app/models/PaisesModelo.php
+++ app/models/PaisesModelo.php
@@ -22,7 +22,37 @@ class PaisesModelo extends Llaves{
 		return $this->db->query($sql);
 	}
 
+    public function getTabla($inicio = 1, $tamano = 0) {
+        $sql = "SELECT id,pais ";
+        $sql .= "FROM paises ";
+        $sql .= "WHERE baja=0";
+        if($tamano>0) {
+            $sql .= " LIMIT ".$inicio.", ".$tamano;
+        }
+        return $this->db->querySelect($sql);
+    }
 
-   
+    public function modificar($data){
+
+		if(empty($data)) return false;
+        $sql = "INSERT INTO paises VALUES(0,"; //1. id 
+        $sql.= "'".$data['idTipoUsuario']."', "; //2. tipo
+        $sql.= "'".$data['nombre']."', ";    	//3. nombre
+        $sql.= "'".$data['apellidoPaterno']."', "; //4. apellido paterno
+        $sql.= "'".$data['apellidoMaterno']."', "; //5. Apellido Materno	   
+        $sql.= "'".$data['correo']."', ";    	//6. correo
+        $sql.= "'".$data['clave']."', ";     	//7. clave de acceso
+        $sql.= "'".$data['genero']."', ";    	//8. genero
+        $sql.= "'".$data['telefono']."', ";    	//9. telefono
+        $sql.= "'".$data['fechaNacimiento']."', ";  //10. fecha nacimieto
+        $sql.= "'".$data['estado']."', ";    	//11. estado
+        //
+        $sql.= "'0', ";                          //12. baja lógica
+        $sql.= "'', ";                           //13. fecha login
+        $sql.= "NOW(),";                         //14. fecha alta-creado
+        $sql.= "'', ";                           //15. fecha baja
+        $sql.= "'')";                            //16. fecha modificado                        
+        return $this->db->queryNoSelect($sql);
+	 }
 
 }
