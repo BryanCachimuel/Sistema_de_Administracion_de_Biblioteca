b8d0836ed2a77f9c8ef53cc6b6859e24aa0a7964
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-11 22:19:21 -0500

    Armando la estructura con html y bootrtrap de la tabla que contendra los datos de los paises

--- app/views/paisesCaratulaVista.php
+++ app/views/paisesCaratulaVista.php
@@ -1,3 +1,29 @@
 <?php include("encabezado.php"); ?>
 
+<div class="table-responsive">
+    <table class="table table-striped" width="100%">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Modificar</th>
+                <th>Borrar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                for ($i=0; $i<count($datos['data']); $i++) { 
+                    print "<tr>";
+                    print "<td class='text-left'>".$datos["data"][$i]['id']."</td>";
+                    print "<td class='text-left'>".$datos["data"][$i]['pais']."</td>";
+                    print "<td><a href='".RUTA."paises/modificar/".$datos["data"][$i]["id"]."' class='btn btn-info'>Modificar</a></td>";
+                    print "<td><a href='".RUTA."paises/borrar/".$datos["data"][$i]["id"]."' class='btn btn-danger'>Borrar</a></td>";
+                    print "</tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+    <a href="<?php print RUTA; ?>paises/alta" class="btn btn-success">Dar de alta un país</a>
+</div>
+
 <?php include("piePagina.php"); ?>
