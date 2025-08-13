d23e2e9cbf4fd3981a09bb4d31bc2d5bffa003e9
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-12 22:43:22 -0500

    Modicando la vista para poder adaptar la vista para eliminar un registro de un país

--- app/views/paisesAltaVista.php
+++ app/views/paisesAltaVista.php
@@ -4,14 +4,21 @@
 
     <div class="form-group text-left">
       <label for="pais">* País:</label>
-      <input type="text" name="pais" id="pais" class="form-control" required value="<?php print isset($datos['data']['pais'])?$datos['data']['pais']:''; ?>">
+      <input type="text" name="pais" id="pais" class="form-control" required value="<?php print isset($datos['data']['pais'])?$datos['data']['pais']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
     </div>
 
     <div class="form-group text-left mt-3">
       <input type="hidden" name="id" id="id" value="<?php if (isset($datos['data']['id'])) { print $datos['data']['id']; } else { print ""; } ?>">
-      
+
+      <?php
+      if (isset($datos["baja"])) { ?>
+        <a href="<?php print RUTA; ?>paises/bajaLogica/<?php print $datos['data']['id']; ?>" class="btn btn-danger">Borrar</a>
+        <a href="<?php print RUTA; ?>paises" class="btn btn-danger">Regresar</a>
+        <p><b>Advertencia: una vez borrado el registro, no podrá recuperar la información</b></p>
+      <?php } else { ?> 
       <input type="submit" value="Enviar" class="btn btn-success">
-      <a href="<?php print RUTA; ?>paises" class="btn btn-info">Regresar</a>
+      <a href="<?php print RUTA.'paises/1'; ?>" class="btn btn-success">Regresar</a>
+    <?php } ?> 
     </div>
   </form>
 
