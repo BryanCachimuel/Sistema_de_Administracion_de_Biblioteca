05e146dfe9b0fc629b7d6f96b03a2a89cf7b25c0
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-14 21:18:49 -0500

    Adecuando el script necesario para el método alta para que por el momento se pueda registrar un idioma

--- app/controllers/Idiomas.php
+++ app/controllers/Idiomas.php
@@ -32,8 +32,85 @@ class Idiomas extends Controlador {
 	}
 
 	public function alta() {
-	 
-	   
+		//Definir los arreglos
+	    $data = [];
+	    $errores = array();
+	    $pag = 1;
+		 //Recibimos la información de la vista
+	    if ($_SERVER['REQUEST_METHOD']=="POST") {
+
+	      $id = $_POST['id'] ?? "";
+	      $idioma = Helper::cadena($_POST['idioma'] ?? "");
+	      $pag = $_POST['pag'] ?? "1";
+
+	      // Validamos la información
+	      if(empty($idioma)){
+	        array_push($errores,"El idioma es requerido.");
+	      }
+
+	      if (empty($errores)) { 
+			// Crear arreglo de datos
+			//
+			$data = [
+			 "id" => $id,
+			 "idioma"=>$idioma
+			];      
+	        //Enviamos al modelo
+	        if(trim($id)===""){
+	          //Alta
+	          if ($this->modelo->alta($data)) {
+	            $this->mensaje(
+	          		"Alta de un idioma", 
+	          		"Alta de un idioma", 
+	          		"Se añadió correctamente el idioma: ".$idioma, 
+	          		"idiomas/".$pag, 
+	          		"success"
+	          	);
+	          } else {
+	          	$this->mensaje(
+	          		"Error al añadir un idioma.", 
+	          		"Error al añadir un idioma.", 
+	          		"Error al modificar el idioma: ".$idioma, 
+	          		"idiomas/".$pag, 
+	          		"danger"
+	          	);
+	          }
+	        } else {
+	          //Modificar
+	          if ($this->modelo->modificar($data)) {
+	            $this->mensaje(
+	          		"Modificar el idioma", 
+	          		"Modificar el idioma", 
+	          		"Se modificó correctamente el idioma: ".$idioma,
+	          		"idiomas/".$pag, 
+	          		"success"
+	          	);
+	          } else {
+	          	$this->mensaje(
+	          		"Error al modificar el idioma.", 
+	          		"Error al modificar el idioma.", 
+	          		"Error al modificar el idioma: ".$idioma, 
+	          		"idiomas/".$pag, 
+	          		"danger"
+	          	);
+	          }
+	        }
+	      }
+	    } 
+	    if(!empty($errores) || $_SERVER['REQUEST_METHOD']!="POST" ){
+	    	//Vista Alta
+		    $datos = [
+		      "titulo" => "Alta de un idioma",
+		      "subtitulo" => "Alta de un idioma",
+		      "activo" => "idiomas",
+		      "menu" => true,
+		      "admon" => "admon",
+		      "pag" => $pag,
+		      "errores" => $errores,
+		      "data" => []
+		    ];
+		    $this->vista("idiomasAltaVista",$datos);
+	    }
   	}
 
   	public function borrar($id="",$pag=1) {
