87e3b08d7a745a965fded29150bd20cd91c68d47
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-19 22:52:19 -0500

    Adecuando la vista con sus respectivos inputs para poder registrar una editorial

--- app/views/editorialesAltaVista.php
+++ app/views/editorialesAltaVista.php
@@ -2,32 +2,35 @@
 
   <form action="<?php print RUTA; ?>editoriales/alta/" method="POST">
 
-  <div class="form-group text-left">
-    <label for="idCategoria">* Categoria:</label>
-    <select class="form-control" name="idCategoria" id="idCategoria" 
-        <?php if(isset($datos["baja"])) {
-            print "disabled";
-        } ?>
-        >
-        <option value="void">---Selecciona una categoría---</option>
-        <?php
-            for ($i=0; $i < count($datos["categorias"]); $i++) { 
-            print "<option value='".$datos["categorias"][$i]["id"]."'";
-              if(isset($datos["data"]["idCategoria"]) && $datos["data"]["idCategoria"]==$datos["categorias"][$i]["id"]){
-                print " selected ";
-              }
-            print ">".$datos["categorias"][$i]["clave"].": ".$datos["categorias"][$i]["categoria"]."</option>";
-          } 
-
-        ?>
-    </select>
-  </div>
-
-    <div class="form-group text-left">
-      <label for="tema">* Nombre del Tema:</label>
-      <input type="text" name="tema" id="tema" class="form-control" required value="<?php print isset($datos['data']['tema'])?$datos['data']['tema']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
+   <div class="form-group text-left">
+      <label for="editorial">* Nombre de la editorial:</label>
+      <input type="text" name="editorial" id="editorial" class="form-control" required value="<?php print isset($datos['data']['editorial'])?$datos['data']['editorial']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
     </div>
 
+    <div class="form-group text-left">
+      <label for="pagina">Página web:</label>
+      <input type="url" name="pagina" id="pagina" class="form-control" required value="<?php print isset($datos['data']['pagina'])?$datos['data']['pagina']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?> placeholder="www.paginaweb.com">
+    </div>
+
+    <div class="form-group text-left">
+      <label for="idPais">* País:</label>
+      <select class="form-control" name="idPais" id="idPais" 
+          <?php if(isset($datos["baja"])) {
+              print "disabled";
+          } ?>
+          >
+          <option value="void">---Selecciona una país---</option>
+          <?php
+              for ($i=0; $i < count($datos["paises"]); $i++) { 
+              print "<option value='".$datos["paises"][$i]["id"]."'";
+                if(isset($datos["data"]["idPais"]) && $datos["data"]["idPais"]==$datos["paises"][$i]["id"]){
+                  print " selected ";
+                }
+              print ">".$datos["paises"][$i]["pais"]."</option>";
+            } 
+          ?>
+      </select>
+   </div>
 
     <div class="form-group text-left mt-3">
       <input type="hidden" name="id" id="id" value="<?php if (isset($datos['data']['id'])) { print $datos['data']['id']; } else { print ""; } ?>">
