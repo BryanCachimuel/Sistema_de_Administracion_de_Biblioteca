12952b4a124e53f6aaa69145f5e4c7ae102b7dca
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-23 21:46:51 -0500

    Realizando el maquetado de la vista donde se lista todos los registros de los usuarios

--- app/views/usuariosCaratulaVista.php
+++ app/views/usuariosCaratulaVista.php
@@ -12,18 +12,18 @@
       </tr>
     </thead>
     <tbody>
-      <?php
-      for ($i = 0; $i < count($datos['data']); $i++) {
+       <?php
+        for($i=0; $i<count($datos['data']); $i++){
         print "<tr>";
-        print "<td class='text-left'>" . $datos["data"][$i]['id'] . "</td>";
-        print "<td class='text-left'>" . $datos["data"][$i]['tipo'] . "</td>";
-        print "<td class='text-left'>" . $datos["data"][$i]['estado'] . "</td>";
-        print "<td class='text-left'>" . $datos["data"][$i]['nombre'] . "</td>";
-        print "<td><a href='" . RUTA . "usuarios/modificar/" . $datos["data"][$i]["id"] . "/" . $datos["pag"]["pagina"] . "' class='btn btn-info'>Modificar</a></td>";
-        print "<td><a href='" . RUTA . "usuarios/borrar/" . $datos["data"][$i]["id"] . "/" . $datos["pag"]["pagina"] . "' class='btn btn-danger'>Borrar</a></td>";
+        print "<td class='text-left'>".$datos["data"][$i]['id']."</td>";
+        print "<td class='text-left'>".$datos["data"][$i]['tipoUsuario']."</td>";
+        print "<td class='text-left'>".$datos["data"][$i]['estado']."</td>";
+        print "<td class='text-left'>".$datos["data"][$i]['nombre']."</td>";
+        print "<td><a href='".RUTA."usuarios/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info'>Modificar</a></td>";
+        print "<td><a href='".RUTA."usuarios/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger'>Borrar</a></td>";
         print "</tr>";
-      }
-      ?>
+        }
+    ?>
     </tbody>
   </table>
   <?php include("paginacion.php"); ?>
