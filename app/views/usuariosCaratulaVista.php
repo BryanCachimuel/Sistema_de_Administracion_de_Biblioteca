93a5fb8da3d058c3aefc7bb28fed29deb913ad06
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-26 22:16:54 -0500

    Agregando un boton de tipo enlace en el atributo de estado para poder actualizar el mismo mediante la redirección hacia la vista de actualización de estado

--- app/views/usuariosCaratulaVista.php
+++ app/views/usuariosCaratulaVista.php
@@ -2,23 +2,23 @@
 <div class="table-responsive">
   <table class="table table-striped" width="100%">
     <thead>
-      <tr>
+      <tr class="text-center">
         <th>id</th>
         <th>Tipo Usuario</th>
-        <th>Estado</th>
         <th>Nombre</th>
+        <th>Estado</th>
         <th>Modificar</th>
         <th>Borrar</th>
       </tr>
     </thead>
-    <tbody>
+    <tbody class="text-center">
        <?php
         for($i=0; $i<count($datos['data']); $i++){
         print "<tr>";
         print "<td class='text-left'>".$datos["data"][$i]['id']."</td>";
         print "<td class='text-left'>".$datos["data"][$i]['tipoUsuario']."</td>";
-        print "<td class='text-left'>".$datos["data"][$i]['estado']."</td>";
         print "<td class='text-left'>".$datos["data"][$i]['nombre']."</td>";
+        print "<td class='text-center'><a href='".RUTA."usuarios/estadoCambiar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-warning'>".$datos["data"][$i]['estado']."</a></td>";
         print "<td><a href='".RUTA."usuarios/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info'>Modificar</a></td>";
         print "<td><a href='".RUTA."usuarios/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger'>Borrar</a></td>";
         print "</tr>";
