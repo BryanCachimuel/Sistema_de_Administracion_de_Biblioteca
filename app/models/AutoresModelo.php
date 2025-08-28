adccf15dca3a4b7dc0bfeab5556ee7c2d338eb19
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-27 21:25:43 -0500

    Creando el modelo de autores con sus respectivos atributos y métodos constructor, alata, bajaLogica, getAutorId, getNumRegistros, getTabla y modificar

---
+++ app/models/AutoresModelo.php
@@ -0,0 +1,88 @@
+<?php  
+/**
+ * 
+ */
+class AutoresModelo extends Llaves
+{
+	protected $db="";
+	
+	function __construct()
+	{
+		$this->db = new MySQLdb();
+	}
+
+	public function alta($data='') {
+	  if(empty($data)) return false;
+        $sql = "INSERT INTO autores VALUES(0,"; //1. id 
+        $sql.= "'".$data['idTipoUsuario']."', "; //2. tipo
+        $sql.= "'".$data['nombre']."', ";    	//3. nombre
+        $sql.= "'".$data['apellidoPaterno']."', "; //4. apellido paterno
+        $sql.= "'".$data['apellidoMaterno']."', "; //5. Apellido Materno	   
+        $sql.= "'".$data['correo']."', ";    	//6. correo
+        $sql.= "'".$data['clave']."', ";     	//7. clave de acceso
+        $sql.= "'".$data['genero']."', ";    	//8. genero
+        $sql.= "'".$data['telefono']."', ";    	//9. telefono
+        $sql.= "'".$data['fechaNacimiento']."', ";  //10. fecha nacimieto
+        $sql.= "'".$data['estado']."', ";    	//11. estado
+        //
+        $sql.= "'0', ";                          //12. baja lógica
+        $sql.= "'', ";                           //13. fecha login
+        $sql.= "NOW(),";                         //14. fecha alta-creado
+        $sql.= "'', ";                           //15. fecha baja
+        $sql.= "'')";                            //16. fecha modificado                        
+        return $this->db->queryNoSelect($sql);
+	}
+
+	public function bajaLogica($id){
+		$sql = "UPDATE autores SET baja=1, baja_dt=(NOW()) WHERE id=".$id;
+		return $this->db->queryNoSelect($sql);
+	}
+
+	public function getAutoresId($id='') {
+		if(empty($id)) return false;
+		$sql = "SELECT * FROM autores WHERE id='".$id."'";
+		return $this->db->query($sql);
+	}
+
+	public function getNumRegistros() {
+		$sql = "SELECT COUNT(*) FROM autores WHERE baja=0";
+		$salida = $this->db->query($sql);
+		return $salida["COUNT(*)"];
+	}
+
+
+	public function getTabla($inicio=1, $tamano=0) {
+		$sql = "SELECT a.id, p.pais, ";
+		$sql.= "a.apellidoPaterno, a.apellidoMaterno, a.nombre ";
+		$sql.= "FROM autores as a, paises as p ";
+		$sql.= "WHERE a.baja=0 AND ";
+		$sql.= "a.idPais=p.id";
+		if ($tamano>0) {
+			$sql.= " LIMIT ".$inicio.", ".$tamano;
+		}
+		return $this->db->querySelect($sql);
+	}
+
+	public function modificar($data) {
+		$salida = false;
+	    if (!empty($data["id"])) {
+	     $sql = "UPDATE autores SET "; 
+	     $sql.= "idTipoUsuario='".$data['idTipoUsuario']."', ";
+	     $sql.= "correo='".$data['correo']."', ";
+	     $sql.= "nombre='".$data['nombre']."', ";
+	     $sql.= "clave='".$data['clave']."', ";
+	     $sql.= "apellidoPaterno='".$data['apellidoPaterno']."', ";
+	     $sql.= "apellidoMaterno='".$data['apellidoMaterno']."', ";
+		 $sql.= "genero='".$data['genero']."', ";
+	     $sql.= "telefono='".$data['telefono']."', ";
+	     $sql.= "fechaNacimiento='".$data['fechaNacimiento']."', ";
+	     $sql.= "estado='".$data['estado']."', ";	
+	     $sql.= "modifica_dt=(NOW()) ";
+	     $sql.= "WHERE id=".$data['id'];
+	     //Enviamos a la base de datos
+	     $salida = $this->db->queryNoSelect($sql);
+	    }
+	    return $salida;
+	}
+}
+?>
