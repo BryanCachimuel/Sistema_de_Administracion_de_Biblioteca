194b272244cd48c4f24131a8cfc6951f9a981e10
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-28 22:05:47 -0500

    Creando la vista para poder listar los autores con sus respectivos libros y sus datos pertenencientes

---
+++ app/views/autoresLibrosCaratulaVista.php
@@ -0,0 +1,30 @@
+<?php include("encabezado.php"); ?>
+<div class="table-responsive">
+    <table class="table table-striped" width="100%">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Tema</th>
+                <th>Título</th>
+                <th>Copias</th>
+                <th>Borrar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            for ($i = 0; $i < count($datos['data']); $i++) {
+                print "<tr>";
+                print "<td class='text-left'>" . $datos["data"][$i]['id'] . "</td>";
+                print "<td class='text-left'>" . $datos["data"][$i]['tema'] . "</td>";
+                print "<td class='text-left'>" . $datos["data"][$i]['titulo'] . "</td>";
+                print "<td><a href='" . RUTA . "autores/copias/" . $datos["data"][$i]["id"] . "' class='btn btn-warning'>Copias</a></td>";
+                print "<td><a href='" . RUTA . "autores/borrar/" . $datos["data"][$i]["id"] . "' class='btn btn-danger'>Borrar</a></td>";
+                print "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+    <?php print "<a href='" . RUTA . "autores/autoresLibrosAlta/" . $datos["idAutor"] . "' class='btn btn-success'>Dar de alta un libro</a>"; ?>
+    <a href="<?php print RUTA; ?>autores" class="btn btn-info">Regresar</a>
+</div>
+<?php include("piepagina.php"); ?>
