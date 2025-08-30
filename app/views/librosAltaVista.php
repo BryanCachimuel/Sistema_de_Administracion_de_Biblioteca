88509e7d3292d663d5fba0d580d9f0d3880596bb
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-29 21:10:33 -0500

    Implementando los inputs adecuados para registrar un nuevo librto con sus respectivos atributos

--- app/views/librosAltaVista.php
+++ app/views/librosAltaVista.php
@@ -1,54 +1,43 @@
 <?php include_once("encabezado.php"); ?>
-  <form action="<?php print RUTA; ?>autores/alta/" method="POST">
+  <form action="<?php print RUTA; ?>libros/alta/" method="POST">
 
     <div class="form-group text-left">
-      <label for="nombre">* Nombre(s):</label>
-      <input type="text" name="nombre" id="nombre" class="form-control"
-      placeholder="Escribe el nombre del usuario." required value="<?php print isset($datos['data']['nombre'])?$datos['data']['nombre']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
-    </div>
-
-    <div class="form-group text-left">
-      <label for="apellidoPaterno">* Apellido paterno:</label>
-      <input type="text" name="apellidoPaterno" id="apellidoPaterno" class="form-control"
-      placeholder="Escribe el apellido paterno del usuario." required value="<?php print isset($datos['data']['apellidoPaterno'])?$datos['data']['apellidoPaterno']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
-    </div>
-
-    <div class="form-group text-left">
-      <label for="apellidoMaterno">Apellido materno:</label>
-      <input type="text" name="apellidoMaterno" id="apellidoMaterno" class="form-control"
-      placeholder="Escribe el apellido paterno del usuario." value="<?php print isset($datos['data']['apellidoMaterno'])?$datos['data']['apellidoMaterno']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
-    </div>
-
-    <div class="form-group text-left">
-      <label for="genero">* Género:</label>
-      <select class="form-control" name="genero" id="genero" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
-      <option value="void">---Selecciona un género---</option>
+      <label for="idTema">* Tema:</label>
+      <select class="form-control" name="idTema" id="idTema" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
+      <option value="void">---Selecciona un tema---</option>
         <?php
-          for ($i=0; $i < count($datos["genero"]); $i++) { 
-            print "<option value='".$datos["genero"][$i]["id"]."'";
-            if(isset($datos["data"]["idGenero"]) && $datos["data"]["idGenero"]==$datos["genero"][$i]["id"]){
+          for ($i=0; $i < count($datos["temas"]); $i++) { 
+            print "<option value='".$datos["temas"][$i]["id"]."'";
+            if(isset($datos["data"]["idTema"]) && $datos["data"]["idTema"]==$datos["temas"][$i]["id"]){
               print " selected ";
             }
-            print ">".$datos["genero"][$i]["genero"]."</option>";
+            print ">".$datos["temas"][$i]["tema"]."</option>";
           } 
         ?>
       </select>
     </div>
 
-       <div class="form-group text-left mb-3">
-      <label for="idPais">* País de origen:</label>
-      <select class="form-control" name="idPais" id="idPais" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
-      <option value="void">---Selecciona el país de origen---</option>
+    <div class="form-group text-left">
+      <label for="idIdioma">* Idioma principal del libro:</label>
+      <select class="form-control" name="idIdioma" id="idIdioma" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
+      <option value="void">---Selecciona un idioma---</option>
         <?php
-          for ($i=0; $i < count($datos["paises"]); $i++) { 
-            print "<option value='".$datos["paises"][$i]["id"]."'";
-            if(isset($datos["data"]["idPais"]) && $datos["data"]["idPais"]==$datos["paises"][$i]["id"]){
+          for ($i=0; $i < count($datos["idiomas"]); $i++) { 
+            print "<option value='".$datos["idiomas"][$i]["id"]."'";
+            if(isset($datos["data"]["idIdioma"]) && $datos["data"]["idIdioma"]==$datos["idiomas"][$i]["id"]){
               print " selected ";
             }
-            print ">".$datos["paises"][$i]["pais"]."</option>";
+            print ">".$datos["idiomas"][$i]["idioma"]."</option>";
           } 
         ?>
       </select>
+    </div>
+
+
+    <div class="form-group text-left mb-3">
+      <label for="titulo">* Título:</label>
+      <input type="text" name="titulo" id="titulo" class="form-control"
+      placeholder="Escribe el título del libro." required value="<?php print isset($datos['data']['titulo'])?$datos['data']['titulo']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
     </div>
 
     <div class="form-group text-left">
@@ -57,12 +46,12 @@
 
       <?php
       if (isset($datos["baja"])) { ?>
-        <a href="<?php print RUTA; ?>autores/bajaLogica/<?php print $datos['data']['id'].'/'.$datos['pag']; ?>" class="btn btn-danger">Borrar</a>
-        <a href="<?php print RUTA.'autores/'.$datos['pag'];?>" class="btn btn-danger">Regresar</a>
+        <a href="<?php print RUTA; ?>libros/bajaLogica/<?php print $datos['data']['id'].'/'.$datos['pag']; ?>" class="btn btn-danger">Borrar</a>
+        <a href="<?php print RUTA.'libros/'.$datos['pag'];?>" class="btn btn-danger">Regresar</a>
         <p><b>Advertencia: una vez borrado el registro, no podrá recuperar la información</b></p>
       <?php } else { ?> 
       <input type="submit" value="Enviar" class="btn btn-success">
-      <a href="<?php print RUTA.'autores/'.$datos['pag']; ?>" class="btn btn-success">Regresar</a>
+      <a href="<?php print RUTA.'libros/'.$datos['pag']; ?>" class="btn btn-success">Regresar</a>
     <?php } ?> 
     </div>
   </form>
