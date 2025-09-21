2fe975be353bbf29b848ff1a24e6050a62870c1b
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-20 21:47:13 -0500

    Creando la vista para listar los registros necesarios de copias con sus respectivos atributos

---
+++ app/views/copiasCaratulaVista.php
@@ -0,0 +1,35 @@
+<?php include("encabezado.php"); ?>
+<div class="table-responsive">
+  <table class="table table-striped" width="100%">
+    <thead>
+      <tr class="text-center">
+        <th>id</th>
+        <th>Clave</th>
+        <th>Título</th>
+        <th>Edición</th>
+        <th>Copia</th>
+        <th>Modificar</th>
+        <th>Borrar</th>
+      </tr>
+    </thead>
+    <tbody class="text-center">
+       <?php
+        for($i=0; $i<count($datos['data']); $i++){
+        print "<tr>";
+        print "<td>".$datos["data"][$i]['id']."</td>";
+        print "<td>".$datos["data"][$i]['clave']."</td>";
+        print "<td>".$datos["data"][$i]['titulo']."</td>";
+        print "<td>".$datos["data"][$i]['edicion']."</td>";
+        print "<td>".$datos["data"][$i]['copia']."</td>";
+        print "<td><a href='".RUTA."copias/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info'>Modificar</a></td>";
+        print "<td><a href='".RUTA."copias/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger'>Borrar</a></td>";
+        print "</tr>";
+        }
+    ?>
+    </tbody>
+  </table>
+  <?php include("paginacion.php"); ?>
+  <a href="<?php print RUTA; ?>copias/alta" class="btn btn-success">
+    Dar de alta una copia</a>
+</div>
+<?php include("piepagina.php"); ?>
