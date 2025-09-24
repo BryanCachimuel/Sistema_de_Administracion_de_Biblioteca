192a46c4124af362b5300a7787408aeec9356352
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-23 21:50:16 -0500

    Agregando el boton correspondiente a copias dentro de la tabla de registros de los libros

--- app/views/librosCaratulaVista.php
+++ app/views/librosCaratulaVista.php
@@ -7,6 +7,7 @@
         <th>Tema</th>
         <th>Título</th>
         <th>Idioma</th>
+        <th>Copias</th>
         <th>Autor(es)</th>
         <th>Modificar</th>
         <th>Borrar</th>
@@ -20,6 +21,7 @@
         print "<td>".$datos["data"][$i]['tema']."</td>";
         print "<td>".$datos["data"][$i]['titulo']."</td>";
         print "<td>".$datos["data"][$i]['idioma']."</td>";
+        print "<td><a href='".RUTA."libros/copias/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-primary'>Copias</a></td>";
         print "<td><a href='".RUTA."libros/librosAutores/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-warning'>Autores</a></td>";
         print "<td><a href='".RUTA."libros/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info'>Modificar</a></td>";
         print "<td><a href='".RUTA."libros/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger'>Borrar</a></td>";
