a9dfd9e32d54dae36b90b309c84d41434d1d5562
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-24 22:13:26 -0500

    Agregando una constante para la función getTabla de prestamos

--- app/inicio.php
+++ app/inicio.php
@@ -1,16 +1,10 @@
 <?php
-// control de las rutas mediante una constante
 define("RUTA", "/biblioteca/");
-
-// Llaves para encriptación de datos del usuario
 define("LLAVE1","Hombresneciosque");
 define("LLAVE2","acusaisalamujer");
-define("CLAVE","clavesecretamaxima");
-
-// paginanción
+define("CLAVE","mimamamemimamucho");
 define("TAMANO_PAGINA",6);
-define("PAGINAS_MAXIMAS",4);
-
+define('PAGINAS_MAXIMAS',4);
 //
 define('ADMON',1);
 define('PROFESOR',2);
@@ -23,17 +17,18 @@ define('USUARIO_ACTIVO',1);
 define('USUARIO_INACTIVO',2);
 define('USUARIO_SUSPENDIDO',3);
 //
-//Estado copias
+//Estados copias
 //
-define('DISPONIBLE',1);
-define('PRESTADO',2);
-define('REPARACION',3);
-define('PERDIDO',4);
-define('NO_DISPONIBLE',5);
-
+define('COPIA_DISPONIBLE',1);
+define('COPIA_PRESTADO',2);
+define('COPIA_REPARACION',3);
+define('COPIA_PERDIDO',4);
+define('COPIA_NO_DISPONIBLE',5);
+//
 require_once("libs/MySQLdb.php");
 require_once("libs/Helper.php");
 require_once("libs/Sesion.php");
 require_once("libs/Llaves.php");
 require_once("libs/Controlador.php");
 require_once("libs/Control.php");
+?>
