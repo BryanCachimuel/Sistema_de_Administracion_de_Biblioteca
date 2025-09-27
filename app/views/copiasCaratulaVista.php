2bd8a071fb26546ce164d529ff9bb4b292e61281
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-26 20:46:03 -0500

    Realizando una validación para los botones de la tabla de copias para que en caso de que una copia este disponible, prestada o reparación se presente con un color determinado o que se desabilite las opciones de modificar o borrar si la copia del libro esta prestada

--- app/views/copiasCaratulaVista.php
+++ app/views/copiasCaratulaVista.php
@@ -1,37 +1,54 @@
 <?php include("encabezado.php"); ?>
-<div class="table-responsive">
+  <div class="table-responsive">
   <table class="table table-striped" width="100%">
-    <thead>
-      <tr class="text-center">
-        <th>id</th>
-        <th>Clave</th>
-        <th>Título</th>
-        <th>Edición</th>
-        <th>Copia</th>
-        <th>Estado</th>
-        <th>Modificar</th>
-        <th>Borrar</th>
-      </tr>
-    </thead>
-    <tbody class="text-center">
-       <?php
-        for($i=0; $i<count($datos['data']); $i++){
-        print "<tr>";
-        print "<td>".$datos["data"][$i]['id']."</td>";
-        print "<td>".$datos["data"][$i]['clave']."</td>";
-        print "<td>".$datos["data"][$i]['titulo']."</td>";
-        print "<td>".$datos["data"][$i]['edicion']."</td>";
-        print "<td>".$datos["data"][$i]['copia']."</td>";
-        print "<td>".$datos["data"][$i]['estadoCopia']."</td>";
-        print "<td><a href='".RUTA."copias/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info'>Modificar</a></td>";
-        print "<td><a href='".RUTA."copias/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger'>Borrar</a></td>";
-        print "</tr>";
-        }
+  <thead>
+    <tr>
+    <th>id</th>
+    <th>Clave</th>
+    <th>Titulo</th>
+    <th>Edición</th>
+    <th>Copia</th>
+    <th>Estado</th>
+    <th>Modificar</th>
+    <th>Borrar</th>
+  </tr>
+  </thead>
+  <tbody>
+    <?php
+    for($i=0; $i<count($datos['data']); $i++){
+      print "<tr>";
+      print "<td class='text-left'>".$datos["data"][$i]['id']."</td>";
+      print "<td class='text-left'>".$datos["data"][$i]['clave']."</td>";
+      print "<td class='text-left'>".$datos["data"][$i]['titulo']."</td>";
+      print "<td class='text-left'>".$datos["data"][$i]['edicion']."</td>";
+      print "<td class='text-left'>".$datos["data"][$i]['copia']."</td>";
+      print "<td class='text-left ";
+      if ($datos["data"][$i]['estado']==COPIA_DISPONIBLE) {
+        print " text-success";
+      } else if ($datos["data"][$i]['estado']==COPIA_PRESTADO) {
+        print " text-info";
+      } else {
+        print " text-danger";
+      }
+      print "'>".$datos["data"][$i]['estadoCopia']."</td>";
+      print "<td><a href='".RUTA."copias/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info";
+      if ($datos["data"][$i]['estado']==COPIA_PRESTADO) {
+        print " disabled";
+      }
+      print "'>Modificar</a></td>";
+      print "<td><a href='".RUTA."copias/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger";
+      if ($datos["data"][$i]['estado']==COPIA_PRESTADO) {
+          print " disabled";
+      }
+      print "'>Borrar</a></td>";
+      print "</tr>";
+    }
     ?>
-    </tbody>
+  </tbody>
   </table>
-  <?php include("paginacion.php"); ?>
-  <a href="<?php print RUTA; ?>copias/alta" class="btn btn-success">
-    Dar de alta una copia</a>
-</div>
+<?php include("paginacion.php"); ?>
+<a href="<?php print RUTA; ?>copias/alta" class="btn btn-success">
+  Dar de alta una copia</a>
+  </div>
+  <p class="mt-3">* Las copias <b>prestadas</b> no pueden ser modificadas ni borradas.</p>
 <?php include("piepagina.php"); ?>
