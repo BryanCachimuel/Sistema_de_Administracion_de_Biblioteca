07efb6b3e201698cc28036432d950b895c9f9192
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-28 21:33:51 -0500

    Creando la función getIntegridadReferencial para que mediante una consulta sql se determine si hay referencia con la entidad prestamos

--- app/models/CopiasModelo.php
+++ app/models/CopiasModelo.php
@@ -77,6 +77,14 @@ class CopiasModelo extends Llaves
 		return $this->db->querySelect($sql);
 	}
 
+	public function getIntegridadReferencial($id) {
+		$ir_array = [0];
+		$sql = "SELECT COUNT(*) FROM prestamos WHERE baja=0 AND idCopia=".$id;
+		$salida = $this->db->query($sql);
+		$ir_array[0] = $salida["COUNT(*)"];
+		return $ir_array;
+	}
+
 
 	public function modificar($data) {
 		$salida = false;
