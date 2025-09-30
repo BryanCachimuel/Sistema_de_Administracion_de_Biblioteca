1ff7619cbe157902435bccd42f7723c8de3ab762
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-29 21:55:19 -0500

    Creando la función getIntegridadReferencial para que mediante una consulta sql se determine si hay referencias entre las entidades temas y libros

--- app/models/TemasModelo.php
+++ app/models/TemasModelo.php
@@ -57,6 +57,14 @@ class TemasModelo extends Llaves{
         return $this->db->querySelect($sql);
     }
 
+    public function getIntegridadReferencial($id) {
+        $ir_array = [0];
+		$sql = "SELECT COUNT(*) FROM libros WHERE baja=0 AND idTema=".$id;
+		$salida = $this->db->query($sql);
+		$ir_array[0] = $salida["COUNT(*)"];
+		return $ir_array;
+    }
+
     public function modificar($data){
         $salida = false;
         if(!empty($data["id"])) {
