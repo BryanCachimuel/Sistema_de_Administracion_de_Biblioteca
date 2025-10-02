c664c78bd23e6bff85fe0a224441f924f7eb4c51
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-01 21:57:53 -0500

    Creando la función getPrestamos la cual contiene una consulta sql para obtener los prestamos de usuario que a iniciado sesión

--- app/models/ConsultarModelo.php
+++ app/models/ConsultarModelo.php
@@ -1,9 +1,27 @@
 <?php
 
-class ConsultarModelo {
+class ConsultarModelo
+{
     private $db = "";
 
-    function __construct() {
+    function __construct()
+    {
         $this->db = new MySQLdb();
+    }
+
+    public function getPrestamos($idUsuario)  {
+        $sql = "SELECT p.id, c.clave, l.titulo, ";
+		$sql.= "DATE_FORMAT(p.prestamo_dt,'%d-%m-%Y') as fechaInicio, ";
+		$sql.= "DATE_FORMAT(p.devolucion_dt,'%d-%m-%Y') as fechaFin ";
+		$sql.= "FROM prestamos as p, usuarios as u, libros as l, copias as c ";
+		$sql.= "WHERE p.baja=0 AND ";
+		$sql.= "p.idCopia=c.id AND ";
+		$sql.= "p.idUsuario=u.id AND ";
+		$sql.= "p.idUsuario=".$idUsuario." AND ";
+		$sql.= "c.idLibro=l.id ";
+		$sql.= "ORDER BY p.devolucion_dt DESC ";
+		$sql.= "LIMIT 10";
+		return $this->db->querySelect($sql);
+
     }
 }
