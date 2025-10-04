07fd0763b81b379d28b4038e0dae469833211362
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-03 22:05:52 -0500

    Creando la función consulta para poder obtener los valores de los inputs del formulario de consultas

--- app/controllers/Consultar.php
+++ app/controllers/Consultar.php
@@ -44,6 +44,39 @@ class Consultar extends Controlador {
         $this->vista("consultarConsultarVista",$datos);
     }
 
+    public function consulta() {
+        $errores = [];
+        if($_SERVER['REQUEST_METHOD'] == "POST") {
+            $idTema = Helper::cadena($_POST['idTema']??"");
+            $nombre = Helper::cadena($_POST['nombre']??"");
+            $apellidoPaterno = Helper::cadena($_POST['apellidoPaterno']??"");
+            $apellidoMaterno = Helper::cadena($_POST['apellidoMaterno']??"");
+            $titulo = Helper::cadena($_POST['titulo']);
+
+            if(empty($nombre) && empty($apellidoPaterno) && empty($apellidoMaterno) && empty($titulo) && $idTema == "void") {
+                $this->caratula();
+            } else {
+                $data = [
+                    "idTema" => $idTema,
+                    "nombre" => $nombre,
+                    "apellidoPaterno" => $apellidoPaterno,
+                    "apellidoMaterno" => $apellidoMaterno,
+                    "titulo" => $titulo
+                ];
+                $data = $this->modelo->getLibros($data);
+
+                $datos = [
+                    "titulo" => "Consultas",
+                    "subtitulo" => "Consultas",
+                    "admon" => false,
+                    "data" => $data,
+                    "menu" => true
+                ];
+                $this->vista("consultarLibrosVista", $datos);
+            }
+        }
+    }
+
     public function logout() {
         if(isset($_SESSION['usuario'])) {
             $this->sesion->finalizarLogin();
