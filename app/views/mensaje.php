2c94d234322edf91ae565548d143d81214f8ba19
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-04 21:29:20 -0500

    Agregando un boton para poder realizar el proceso de respaldo de la base de datos

--- app/views/mensaje.php
+++ app/views/mensaje.php
@@ -1,8 +1,13 @@
-<?php include_once("encabezado.php"); 
+<?php include_once("encabezado.php");
 
-print '<div class="alert '.$datos["color"].'"mt-3>';
-print '<h4>'.$datos["texto"].'</h4>';
+print '<div class="alert ' . $datos["color"] . '" mt-3>';
+print '<h4>' . $datos["texto"] . '</h4>';
 print '</div>';
-print '<a href="'.RUTA.$datos["url"].'" class="btn '.$datos["colorBoton"].'" >';
-print $datos["textoBoton"].'</a>';
-include_once("piePagina.php"); ?>
+if (isset($datos["url2"]) && !empty($datos["url2"])) {
+    print '<a href="' . RUTA . $datos["url2"] . '" class="btn ' . $datos["colorBoton2"] . '" >';
+    print $datos["textoBoton2"] . '</a>&nbsp;';
+}
+print '<a href="' . RUTA . $datos["url"] . '" class="btn ' . $datos["colorBoton"] . '" >';
+print $datos["textoBoton"] . '</a>';
+
+include_once("piepagina.php");
