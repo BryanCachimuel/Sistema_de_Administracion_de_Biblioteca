14ee2433497051503deb718b2abaf3aa192b3f76
BryanCachimuel <bryanloyo56@gmail.com>
2025-12-03 22:45:55 -0500

    Agrangando los iconos en la vista de olvido de contraseña

--- app/views/loginOlvidoVista.php
+++ app/views/loginOlvidoVista.php
@@ -3,13 +3,13 @@
 <form action="<?php print RUTA; ?>login/olvidoVerificar" method="POST" autocomplete="off">
 
     <div class="form-group text-left">
-        <label for="usuario">* Correo:</label>
+        <label for="usuario">* <i class="fa-regular fa-envelope"></i> Correo:</label>
         <input type="email" class="form-control" name="usuario" placeholder="Escribe tu correo electrónico" required>
     </div>
 
     <div class="form-group text-left mt-2">
         <input type="submit" value="Enviar" class="btn btn-success">
-        <a href="<?php print RUTA; ?>login/caratula" type="button" class="btn btn-info">Regresar</a>
+        <a href="<?php print RUTA; ?>login/caratula" type="button" class="btn btn-info"><i class="fa-solid fa-angles-left"></i> Regresar</a>
     </div>
 </form>
 
