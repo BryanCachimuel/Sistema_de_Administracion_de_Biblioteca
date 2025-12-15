76cc0bb1267edda00c98e754e0f04f3249afaa94
BryanCachimuel <bryanloyo56@gmail.com>
2025-12-14 23:07:45 -0500

    Agregando iconos y espaciado adecuado entre los inputs del formulario de modificación de estado de los usuarios

--- app/vistas/usuariosAltaVista.php
+++ app/vistas/usuariosAltaVista.php
@@ -2,30 +2,30 @@
   <form action="<?php print RUTA; ?>usuarios/alta/" method="POST">
 
     <div class="form-group text-left">
-      <label for="correo">* Correo (usuario):</label>
+      <label for="correo">* <i class="fa-regular fa-envelope"></i> Correo (usuario):</label>
       <input type="email" name="correo" id="correo" class="form-control" required value="<?php print isset($datos['data']['correo'])?$datos['data']['correo']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
     </div>
 
     <div class="form-group text-left">
-      <label for="nombre">* Nombre(s):</label>
+      <label for="nombre">* <i class="fa-regular fa-id-badge"></i> Nombre(s):</label>
       <input type="text" name="nombre" id="nombre" class="form-control"
       placeholder="Escribe el nombre del usuario." required value="<?php print isset($datos['data']['nombre'])?$datos['data']['nombre']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
     </div>
 
     <div class="form-group text-left">
-      <label for="apellidoPaterno">* Apellido paterno:</label>
+      <label for="apellidoPaterno">* <i class="fa-regular fa-id-badge"></i> Apellido paterno:</label>
       <input type="text" name="apellidoPaterno" id="apellidoPaterno" class="form-control"
       placeholder="Escribe el apellido paterno del usuario." required value="<?php print isset($datos['data']['apellidoPaterno'])?$datos['data']['apellidoPaterno']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
     </div>
 
     <div class="form-group text-left">
-      <label for="apellidoMaterno">Apellido materno:</label>
+      <label for="apellidoMaterno"><i class="fa-regular fa-id-badge"></i> Apellido materno:</label>
       <input type="text" name="apellidoMaterno" id="apellidoMaterno" class="form-control"
       placeholder="Escribe el apellido paterno del usuario." value="<?php print isset($datos['data']['apellidoMaterno'])?$datos['data']['apellidoMaterno']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
     </div>
 
     <div class="form-group text-left">
-      <label for="genero">* Género:</label>
+      <label for="genero">* <i class="fa-solid fa-people-arrows"></i> Género:</label>
       <select class="form-control" name="genero" id="genero" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
       <option value="void">---Selecciona un género---</option>
         <?php
@@ -41,19 +41,19 @@
     </div>
 
     <div class="form-group text-left">
-      <label for="telefono">Teléfono:</label>
+      <label for="telefono"><i class="fa-solid fa-mobile-screen"></i> Teléfono:</label>
       <input type="text" name="telefono" id="telefono" class="form-control"
       placeholder="Escribe el telefono del usuario." value="<?php print isset($datos['data']['telefono'])?$datos['data']['telefono']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
     </div>
 
     <div class="form-group text-left">
-      <label for="fechaNacimiento">Fecha nacimiento:</label>
+      <label for="fechaNacimiento"><i class="fa-regular fa-calendar-days"></i> Fecha nacimiento:</label>
       <input type="date" name="fechaNacimiento" id="fechaNacimiento" class="form-control"
       placeholder="Seleccione la fecha de nacimiento del usuario." value="<?php print isset($datos['data']['fechaNacimiento'])?$datos['data']['fechaNacimiento']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
     </div>
 
     <div class="form-group text-left">
-      <label for="idTipoUsuario">* Tipo de usuario:</label>
+      <label for="idTipoUsuario">* <i class="fa-solid fa-person-circle-check"></i> Tipo de usuario:</label>
       <select class="form-control" name="idTipoUsuario" id="idTipoUsuario" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
       <option value="void">---Selecciona un tipo de usuario---</option>
         <?php
@@ -69,7 +69,7 @@
     </div>
 
     <div class="form-group text-left">
-      <label for="estado">* Estado usuario:</label>
+      <label for="estado">* <i class="fa-solid fa-chart-bar"></i> Estado usuario:</label>
       <select class="form-control" name="estado" id="estado" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
       <option value="void">---Selecciona un estado de usuario---</option>
         <?php
