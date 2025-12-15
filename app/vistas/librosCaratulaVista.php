adb2555f6575bb96ffd48a28d6c51a7f05809777
BryanCachimuel <bryanloyo56@gmail.com>
2025-12-14 23:14:17 -0500

    Agregando iconos para copias, autores, modificar, borrar y agregar dentro de la vista principal de libros

--- app/vistas/librosCaratulaVista.php
+++ app/vistas/librosCaratulaVista.php
@@ -21,17 +21,15 @@
       print "<td class='text-left'>".$datos["data"][$i]['tema']."</td>";
       print "<td class='text-left'>".$datos["data"][$i]['titulo']."</td>";
       print "<td class='text-left'>".$datos["data"][$i]['idioma']."</td>";
-      print "<td><a href='".RUTA."libros/copias/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-warning'>Copias</a></td>";
-      print "<td><a href='".RUTA."libros/librosAutores/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-warning'>Autor(es)</a></td>";
-      print "<td><a href='".RUTA."libros/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info'>Modificar</a></td>";
-      print "<td><a href='".RUTA."libros/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger'>Borrar</a></td>";
+      print "<td><a href='".RUTA."libros/copias/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-warning'><i class='fa-solid fa-book'></i></a></td>";
+      print "<td><a href='".RUTA."libros/librosAutores/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-warning'><i class='fa-solid fa-users-between-lines'></i></a></td>";
+      print "<td><a href='".RUTA."libros/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info'><i class='fa-solid fa-pen-clip'></i></a></td>";
+      print "<td><a href='".RUTA."libros/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger'><i class='fa-solid fa-trash-can'></i></a></td>";
       print "</tr>";
     }
     ?>
   </tbody>
   </table>
 <?php include("paginacion.php"); ?>
-<a href="<?php print RUTA; ?>libros/alta" class="btn btn-success">
-  Dar de alta un libro</a>
-  </div>
+<a href="<?php print RUTA; ?>libros/alta" class="btn btn-success"><i class="fa-solid fa-circle-plus"></i> Dar de alta un libro</a></div>
 <?php include("piepagina.php"); ?>
