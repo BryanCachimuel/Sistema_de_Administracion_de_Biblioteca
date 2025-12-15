64cb2dc1af56e8e72f35f35cb10680b8b96504d7
BryanCachimuel <bryanloyo56@gmail.com>
2025-12-14 22:57:15 -0500

    Agregando iconos de modificar, borrar y agregar dentro del listado de categorías

--- app/vistas/categoriasCaratulaVista.php
+++ app/vistas/categoriasCaratulaVista.php
@@ -17,15 +17,13 @@
       print "<td class='text-left'>".$datos["data"][$i]['id']."</td>";
       print "<td class='text-left'>".$datos["data"][$i]['clave']."</td>";
       print "<td class='text-left'>".$datos["data"][$i]['categoria']."</td>";
-      print "<td><a href='".RUTA."categorias/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info'>Modificar</a></td>";
-      print "<td><a href='".RUTA."categorias/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger'>Borrar</a></td>";
+      print "<td><a href='".RUTA."categorias/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info'><i class='fa-solid fa-pen-clip'></i></a></td>";
+      print "<td><a href='".RUTA."categorias/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger'><i class='fa-solid fa-trash-can'></i></a></td>";
       print "</tr>";
     }
     ?>
   </tbody>
   </table>
 <?php include("paginacion.php"); ?>
-<a href="<?php print RUTA; ?>categorias/alta" class="btn btn-success">
-  Dar de alta una categoría</a>
-  </div>
+<a href="<?php print RUTA; ?>categorias/alta" class="btn btn-success"><i class="fa-solid fa-circle-plus"></i> Dar de alta una categoría</a></div>
 <?php include("piepagina.php"); ?>
